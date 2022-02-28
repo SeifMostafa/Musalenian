c607d7322a107639f84e1ab26a667ca6a4df4853
Seif . Mostafa <seif.mostafa@MUSALA.COM>
2022-02-28 17:52:50 +0200

    sync, async producer methods implemented.

--- src/main/java/com/musala/MDS/Producer.java
+++ src/main/java/com/musala/MDS/Producer.java
@@ -1,15 +1,63 @@
 package com.musala.MDS;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.musala.MDS.entity.Musalenian;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.concurrent.ExecutionException;
 
 @Component
 @Slf4j
 public class Producer {
 
     @Autowired
-    KafkaTemplate<Integer,String> kafkaTemplate;
+    KafkaTemplate<Integer, String> kafkaTemplate;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    public void register(Musalenian musalenian) throws JsonProcessingException {
+        Integer key = Integer.valueOf(musalenian.getId().intValue());
+        String value = objectMapper.writeValueAsString(musalenian);
+        ListenableFuture<SendResult<Integer, String>> sendResultListenableFuture = kafkaTemplate.sendDefault(key,
+                value);
+        sendResultListenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                handleFailure(key, value, ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<Integer, String> result) {
+                handleSuccess(key, value, result);
+            }
+        });
+    }
+
+    public void registerSync(Musalenian musalenian) throws JsonProcessingException, ExecutionException, InterruptedException {
+      kafkaTemplate.sendDefault(Integer.valueOf(musalenian.getId().intValue()),
+                objectMapper.writeValueAsString(musalenian)).get();
+
+    }
+
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+        log.error("Error Sending the Message and the exception is {}", ex.getMessage());
+        try {
+            throw ex;
+        } catch (Throwable throwable) {
+            log.error("Error in OnFailure: {}", throwable.getMessage());
+        }
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
+        log.info("Message Sent SuccessFully for the key : {} and the value is {} , partition is {}", key, value, result.getRecordMetadata().partition());
+    }
 }
--- src/main/java/com/musala/MDS/controller/RegistrationController.java
+++ src/main/java/com/musala/MDS/controller/RegistrationController.java
@@ -1,9 +1,43 @@
 package com.musala.MDS.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.musala.MDS.Producer;
+import com.musala.MDS.entity.Musalenian;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/registration")
 public class RegistrationController {
 
 
+    @Autowired
+    Producer producer;
+    /*
+    to allow user to register
+     */
+    @PostMapping
+    public ResponseEntity<Musalenian> register(@RequestBody Musalenian musalenian) throws JsonProcessingException {
+        /// invoke kafka producer  -> insert into database --> return response entity
+        producer.register(musalenian);
+        return ResponseEntity.status(HttpStatus.CREATED).body(musalenian);
+    }
+
+    /*
+    to allow user to modify registration
+     */
+    @PutMapping
+    public void modifyRegistration() {
+
+    }
+
+    /*
+    to allow user to delete registration, delete user record
+     */
+    @DeleteMapping
+    public void deleteRegistration() {
+
+    }
 }
